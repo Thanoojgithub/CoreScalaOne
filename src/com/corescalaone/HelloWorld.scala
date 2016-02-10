4f4de0ebfad2b9f12a7b9229c97b6f4669aa9560
thanoojgithub <javaeecub@gmail.com>
2016-02-10 18:07:20 +0530

    initial commit

---
+++ src/com/corescalaone/HelloWorld.scala
@@ -0,0 +1,35 @@
+package com.corescalaone
+
+import scala.io.StdIn
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    // Not mandatory to call functions inside main()
+
+    println("Enter a i value. ")
+    val i = StdIn.readDouble()
+
+    val result = square(i)
+    println("def square :: " + result)
+  }
+
+  def multiple(x: Double, y: Double): Int = { if (x <= 0 || y <= 0) 0 else x.toInt * y.toInt }
+
+  def square(i: Double): Double = { if (i <= 0) 0 else i * i }
+
+  def quadrant(x: Double, y: Double): Int = {
+    if (x < 0) {
+      if (y < 0) 3 else 2
+    } else {
+      if (y < 0) 4 else 1
+    }
+  }
+
+  println("Enter an x-coordinate. ")
+  val x = StdIn.readDouble()
+  println("Enter an y-coordinate. ")
+  val y = StdIn.readDouble()
+  println("def multiple :: " + multiple(x, y))
+  println("def quadrant :: " + quadrant(x, y))
+
+}
