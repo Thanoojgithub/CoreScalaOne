101084b7ef6bad07068fe2f1f71c25a5419fb248
thanoojgithub <javaeecub@gmail.com>
2016-02-22 15:20:15 +0530

    file read and comparable

--- src/com/corescalaone/ComparableOne.scala
+++ src/com/corescalaone/ComparableOne.scala
@@ -4,35 +4,32 @@ import scala.io.Source
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.MutableList
 import java.io.File
-/**
- * val fileLines = io.Source.fromFile("Colors.scala").getLines.toList
- * fileLines.foreach(println)
- *
- * val reader = new FileReader("foo.txt")
- *
- * val writer = new PrintWriter(new File("Write.txt"))
- *
- * writer.write("Hello Developer, Welcome to Scala Programming.")
- * writer.close()
- */
+import java.io.IOException
+import java.io.FileNotFoundException
+
 object ComparableOne {
 
   def main(args: Array[String]): Unit = {
-    val afile: java.io.File = new File("Authors.csv")
-    //exception : java.io.FileNotFoundException: Authors.csv (The system cannot find the file specified)
+    // FileNotFoundException : java.io.FileNotFoundException: Authors.csv (The system cannot find the file specified)
+    val afile: java.io.File = new File("D:/thanooj/work/Workspaces/WSscala/WSOne/CoreScalaNew/resources/Authors.csv")
+
     loadAuthorsFromFile(afile).foreach { println }
   }
 
   def loadAuthorsFromFile(afile: java.io.File): MutableList[Author] = {
     var authorList: MutableList[Author] = MutableList[Author]()
     try {
+      //val fileLines = io.Source.fromFile("Colors.scala").getLines.toList
       for (line <- Source.fromFile(afile).getLines()) {
         val authorArr = line.split(",")
         val author: Author = new Author(authorArr(0).toInt, authorArr(1), authorArr(2))
         authorList += author
       }
     } catch {
-      case ex: Exception => println("exception : " + ex)
+      case ex: FileNotFoundException => println("FileNotFoundException : " + ex)
+      case ex: IOException           => println("IO Exception : " + ex)
+    } finally {
+      println("OK")
     }
     authorList
   }
@@ -50,3 +47,20 @@ class Author(val id: Int, val name: String, val location: String) extends Compar
   }
   override def toString() = id + " " + name + " " + location
 }
+
+
+
+/**
+ * 
+ * NOTE:
+ * 
+ * val fileLines = io.Source.fromFile("Colors.scala").getLines.toList
+ * fileLines.foreach(println)
+ *
+ * val reader = new FileReader("foo.txt")
+ *
+ * val writer = new PrintWriter(new File("Write.txt"))
+ *
+ * writer.write("Hello Developer, Welcome to Scala Programming.")
+ * writer.close()
+ */
